df4c999e36086ded908750705b92d714d4a95b7f
Дмитро <dimarik_p@ukr.net>
2025-03-03 21:39:57 +0200

    Homework 1

---
+++ Lesson 2/mainDZ1.py
@@ -0,0 +1,9 @@
+number = int(input("Please enter a number: "))
+number1 = number % 10
+print(number1)
+number2 = number % 100//10
+print(number2)
+number3 = number // 100%10
+print(number3)
+number4 = number // 1000%10
+print(number4)
