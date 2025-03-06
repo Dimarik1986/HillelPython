af4f567758820717d23817135ec1acfffb4df5e6
Дмитро <dimarik_p@ukr.net>
2025-03-06 10:34:22 +0200

    Homework 3 ListSeparation

---
+++ Lesson 3/Lists/listDZ3.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3,4,5,6,7,8,9]
+
+middle_lists = len(numbers) // 2
+
+if len(numbers) % 2 != 0:
+    middle_lists += 1
+print(middle_lists)
+first_part_lists = numbers[:middle_lists]
+second_part_lists = numbers[middle_lists:]
+
+result = [first_part_lists, second_part_lists]
+print(result)
