17fee7900cbb70e099db2524424cb81035540b73
Дмитро <dimarik_p@ukr.net>
2025-03-12 12:28:59 +0200

    Homework 5 DZ 5.2

--- Lesson 3/Calculate/main.py
+++ Lesson 3/Calculate/main.py
@@ -1,18 +1,24 @@
-number1 = int(input("Please enter first number: "))
-number2 = int(input("Please enter second number: "))
+start_program = "Yes"
 
-operator= input("Please enter mathematical operation: ")
+while start_program == "Yes":
 
-match operator:
-    case "+":
-        result = number1 + number2
-        print(result)
-    case "*":
-        result = number1 * number2
-        print(result)
-    case "/":
-        result = number1 / number2
-        print(result)
-    case "-":
-        result = number1 - number2
-        print(result)
+    number1 = int(input("Please enter first number: "))
+    number2 = int(input("Please enter second number: "))
+
+    operator= input("Please enter mathematical operation: ")
+
+    match operator:
+        case "+":
+            result = number1 + number2
+            print(result)
+        case "*":
+            result = number1 * number2
+            print(result)
+        case "/":
+            result = number1 / number2
+            print(result)
+        case "-":
+            result = number1 - number2
+            print(result)
+
+    start_program = str(input("Let's continue?: "))
