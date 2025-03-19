f6b7bef59631b33d55d99abd4c7c432c322fea4f
Дмитро <dimarik_p@ukr.net>
2025-03-19 16:44:06 +0200

    Homework 5 DZ 6.2

---
+++ Lesson 6/DZ6.2.py
@@ -0,0 +1,15 @@
+
+total_seconds = [0, 224930, 466289, 950400, 1209600, 1900800, 8639999, 22493, 7948799]
+
+for total_second in total_seconds:
+    x = 60 * 60 * 24
+    y = 60 * 60
+
+    days = total_second // x
+
+    hours = total_second % x//y
+
+    minutes = (total_second % 3600) // 60
+    seconds = total_second % 60
+
+    print(f"{total_second}, -> ,{days} days, {hours}:{minutes}:{seconds}")
